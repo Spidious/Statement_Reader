8afb1f79d24e67cf5bc533b7467b0a6276bab203
Luke Deffenbaugh <lukedeffenbaugh@gmail.com>
2026-05-06 15:31:33 -0500

    Implemented terminal ui
    Technically

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ with open('config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.SafeLoader)
 
 # Initialize the transaction categories to 0
-for key in config['transaction-category'].keys():
-    config['transaction-category'][key] = 0
+totals = {}
+for key in config['transaction-category']:
+    totals[key[:3]] = 0.0
 
 for t in statement.get_transactions():
-    user_in = input(t.amount + " : " + t.description + f"\n{[x + f" ({x[:2]})" for x in config['transaction-category'].keys()]}: ")
+    user_in = input("\n" + t.amount + " : " + t.description + f"\n{[x + f" ({x[:3]})" for x in config['transaction-category']]}: ")
     if user_in == "":
         continue
+    totals[user_in] += float(t.amount)
 
 
-print(config)
+print(totals)
